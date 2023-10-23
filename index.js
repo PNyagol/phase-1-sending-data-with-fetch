01b27cddf55fcd6a1fa246af2d66ad50317d449b
Nyagol <nyagolpeter@gmail.com>
2023-10-23 10:32:31 +0300

    still working on the POST request

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(name, email) {
+    const userData = {
+      name: name,
+      email: email
+    };
+  
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(userData)
+    };
+  
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        document.body.innerHTML = data.id;
+      })
+      .catch(error => {
+        document.body.innerHTML = error.message;
+      });
+  }
+ 
+
